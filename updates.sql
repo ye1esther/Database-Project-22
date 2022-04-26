a8c2dbff48883cec711b8cc6cc3eb0803862e332
Kyoungjin Lim <klim30@jhu.edu>
2022-04-25 22:44:29 -0400

    updates.sql updated

--- updates.sql
+++ updates.sql
@@ -9,6 +9,19 @@
    INSERT INTO Content(show_id, type, title, released_country, date_added, release_year, rating, duration)
    VALUES ("s8809", "Movie", "Hairspray", "United States", "2021-10-01 00:00:00", 2007, "PG", "116 min");
    
+-- Influenced_by Table
+
+   INSERT INTO Influenced_by
+        SELECT Content.show_id, Covid.record_id
+        FROM Content, Covid
+        WHERE show_id = "s8808" AND Content.released_country = Covid.country;
+        
+   INSERT INTO Influenced_by
+        SELECT Content.show_id, Covid.record_id
+        FROM Content, Covid
+        WHERE show_id = "s8809" AND Content.released_country = Covid.country;
+
+
 -- Covid Table
 
 -- Financial Table 
@@ -18,6 +31,27 @@
    VALUES ("2022-03-31 00:00:00", 7867.77, 657.53, 1597.45, 3.53);
 
 -- Deletions
-
-
+   DELETE FROM Covid 
+   WHERE Covid.country LIKE "%World%";
+   
+   
+   DELETE FROM Covid 
+   WHERE Covid.country LIKE "%Low income%";
+   
+   
+   DELETE FROM Covid 
+   WHERE Covid.country LIKE "%High income%";
+                    
+   DELETE FROM Covid 
+   WHERE Covid.country LIKE "%Europe%";  
+   
+   DELETE FROM Covid 
+   WHERE Covid.country LIKE "%Asia%";       
+   
+   DELETE FROM Covid 
+   WHERE Covid.country LIKE "%South America%";   
+   
+   DELETE FROM Covid 
+   WHERE Covid.country LIKE "%North America%";        
+   
 
