670cb0d82aafa212c18054cd2dde256386d9290c
Kyoungjin Lim <klim30@jhu.edu>
2022-05-07 20:25:54 -0400

    delete_content.php edited

--- delete_content.php
+++ delete_content.php
@@ -1,8 +1,13 @@
 <?php
 
     $show_id = $_POST['show_id'];
+    $count_Content_before = "SELECT COUNT(show_id) FROM Content";
     $sql = "DELETE FROM Content WHERE show_id = '$show_id'";
+    $count_Content_after = "SELECT COUNT(show_id) FROM Content";
+
+    $count_IB_before = "SELECT COUNT(show_id) FROM Influenced_by";
     $sql2 = "DELETE FROM Influenced_by WHERE show_id = '$show_id'";
+    $count_IB_after = "SELECT COUNT(show_id) FROM Influenced_by";
 
     if (!empty($show_id)) {
 
@@ -10,7 +15,15 @@
         include 'open.php';
 
         if ($conn->query($sql) && $conn->query($sql2)) {
-            echo "Deleted Data Successfully!";
+            if ($count_Content_after == $count_Content_after) { 
+                echo "ERROR: Non existing record";
+            } else { 
+                echo "Deleted Data Successfully!";
+            }
+            //여기 이것도 필요한지 모르겠는데 혹시나 !!
+            if ($count_IB_after == $count_IB_before) { 
+                echo "ERROR: Non existing record";
+            } 
         } else {
             echo "ERROR: ".$sql."<br>".$conn->error;
         }
