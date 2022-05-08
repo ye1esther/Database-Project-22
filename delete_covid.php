05a27e1a87c0fe1f841810376de42831d5465046
Kyoungjin Lim <klim30@jhu.edu>
2022-05-07 21:46:06 -0400

    delete_covid.php edited

--- delete_covid.php
+++ delete_covid.php
@@ -3,16 +3,22 @@
     $country = $_POST['country'];
     $record_date = $_POST['record_date'];
     $sql = "DELETE FROM Covid WHERE country = '$country' AND record_date = '$record_date'";
+    $sql2 = "DELETE FROM Influenced_by WHERE record_id = (SELECT record_id FROM Covid WHERE country = '$country')";
 
-    $sql2 = "DELETE FROM Influenced_by
-                SELECT record_id
-                FROM Covid
-                WHERE country = '$country'";
-
+  
     if (!empty($country) || !empty($record_date)) {
 
         //open a connection to dbase server 
         include 'open.php';
+
+        $valid = $conn->query("SELECT COUNT(*) AS c FROM Covid  WHERE country = '$country' AND record_date = '$record_date'");
+        //extract result from query 
+        $row = $valid->fetch_assoc();
+
+        if ($row['c'] == '0') {
+            echo "ERROR: Record does not exist"; 
+            die(); 
+        }
 
         if ($conn->query($sql) && $conn->query($sql2)) {
             echo "Deleted Data Successfully!";
